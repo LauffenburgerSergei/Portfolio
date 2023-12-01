2c4cf80b591f6eed5054872fc240b056defaff50
LauffenburgerSergei <76686121+LauffenburgerSergei@users.noreply.github.com>
2023-12-01 13:21:24 +0100

    ajout section about - portfolio - contact

--- js/app.js
+++ js/app.js
@@ -41,3 +41,12 @@ window.onscroll = () => {
     }
   });
 };
+
+
+VanillaTilt.init(document.querySelectorAll(".box"), {
+  max: 25,
+  speed: 400,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  perspective: 500,
+  transition: true,
+});
