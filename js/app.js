7533169365c221860338f50148afa150a05f6c80
LauffenburgerSergei <76686121+LauffenburgerSergei@users.noreply.github.com>
2023-12-10 12:51:20 +0100

    creation pages

--- js/app.js
+++ js/app.js
@@ -1,46 +1,21 @@
-// Sélectionne toutes les sections de la page
-let sections = document.querySelectorAll("section");
+document.addEventListener("DOMContentLoaded", function() {
+  // Obtenez l'URL actuelle
+  const currentUrl = window.location.href;
 
-// Sélectionne tous les icônes dans les liens de la barre de navigation
-let navIcons = document.querySelectorAll("header nav a i");
+  // Sélectionnez tous les liens de navigation
+  const navLinks = document.querySelectorAll('.nav__section--link a');
 
-// Ajoute un écouteur d'événements de défilement sur la fenêtre
-window.onscroll = () => {
-  // Parcourt chaque section pour déterminer si elle est actuellement visible
-  sections.forEach((section) => {
-    // Obtient la position verticale actuelle de défilement de la fenêtre
-    let top = window.scrollY;
+  navLinks.forEach(link => {
+    // Supprimez la classe 'active' de tous les liens
+    link.querySelector('i').classList.remove('active');
 
-    // Calcule l'offset (position) de la section actuelle, ajusté de 100 pixels
-    let offset = section.offsetTop - 100;
-
-    // Obtient la hauteur de la section actuelle
-    let height = section.offsetHeight;
-
-    // Obtient l'ID de la section actuelle
-    let id = section.getAttribute("id");
-
-    // Vérifie si la section actuelle est dans la fenêtre d'affichage
-    if (top >= offset && top < offset + height) {
-      // Parcourt chaque icône dans la barre de navigation
-      navIcons.forEach((icon) => {
-        // Enlève la classe 'active' de l'icône (pour désactiver les autres sections)
-        icon.classList.remove("active");
-
-        // Construit un sélecteur pour trouver l'icône correspondant à la section visible
-        let selector = `header nav a[href*=${id}] i`;
-
-        // Utilise le sélecteur pour trouver l'icône correspondante
-        let activeIcon = document.querySelector(selector);
-
-        // Si l'icône correspondante est trouvée, ajoute la classe 'active' à cette icône
-        if (activeIcon) {
-          activeIcon.classList.add("active");
-        }
-      });
+    // Vérifiez si l'URL du lien correspond à l'URL actuelle
+    if (link.href === currentUrl) {
+      // Ajoutez la classe 'active' à l'icône du lien correspondant
+      link.querySelector('i').classList.add('active');
     }
   });
-};
+});
 /*-------------------------------------------------------------------------------------------------------------*/
 /*--------------------------------------------------  VanillaTilt  --------------------------------------------*/
 /*-------------------------------------------------------------------------------------------------------------*/
